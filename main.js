4f0f81967b15532d51a0b1bd24cabd5be11e4da1
andrewnsv <andrejnizovoj598@gmail.com>
2022-12-15 02:36:23 +0200

    fix task 2

--- main.js
+++ main.js
@@ -14,11 +14,13 @@ console.log(fillArray(10, "Slavik"));
 
 let array  = [0, 1, 2, null, undefined, 'qwerty', false];
 
-function filterArray() {  
-  return array.filter((negativeEl) => !!negativeEl);
+function filterArray() {
+  const allArgumets = [...arguments];
+  
+  return array.filter(el => !allArgumets.includes(el));
 }
 
-const result = filterArray(array, false, undefined, '', 0, null); 
+let result = filterArray(array, false, undefined, '', 0, null); 
 console.log(result);
 
 // 3) Напишите функцию calcSum, которая вернет сумму всех входящих параметров функции.
